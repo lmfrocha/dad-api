8d03b187aecacd4706a40043d554b39beddbb2c0
Lucas Marcelino Ferreira Rocha <949137@sga.pucminas.br>
2019-11-21 19:15:23 -0300

    COrreção

--- src/main/java/br/com/pucminas/dad/api/model/Doacao.java
+++ src/main/java/br/com/pucminas/dad/api/model/Doacao.java
@@ -11,6 +11,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name =  "doacao")
 public class Doacao {
@@ -22,6 +25,8 @@ public class Doacao {
 	@Column(name = "VALOR")
 	private Double valor;
 
+	@JsonIgnore
+	@JsonFormat(pattern="dd-MM-yyyy HH-mm-ss")
 	@Column(name = "DIA")
 	private Date data;
 	
