9dabce26293b79b7caf215be09297c278556387d
Lucas Rocha <lucas.marcelino@outlook.com>
2019-11-24 22:59:45 -0300

    Cors liberação

--- src/main/java/br/com/pucminas/dad/api/controller/DoacaoController.java
+++ src/main/java/br/com/pucminas/dad/api/controller/DoacaoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import br.com.pucminas.dad.api.model.Pessoa;
 import br.com.pucminas.dad.api.reposiroty.DoacaoRepository;
 import br.com.pucminas.dad.api.service.ApiService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("doacao")
 public class DoacaoController {
--- src/main/java/br/com/pucminas/dad/api/controller/MainController.java
+++ src/main/java/br/com/pucminas/dad/api/controller/MainController.java
@@ -1,10 +1,12 @@
 package br.com.pucminas.dad.api.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/")
 public class MainController {
